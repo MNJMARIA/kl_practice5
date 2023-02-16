0b077dd2744b116784eec802cc96ac2ad0585777
Maria <stepanovamasha70@gmail.com>
2023-02-16 12:41:12 +0200

    add gitignore

---
+++ src/my_program/Test.java
@@ -0,0 +1,11 @@
+package my_program;
+
+public class Test {
+    public static void main(String[] args) {
+        workWithEnum work = new workWithEnum();
+        work.findMonthByHisNumber();
+        work.findSeasonOfTheMonth();
+
+    }
+}
+
